f3a14e5d55604a702a1f76d2cad958e9ae9a4eac
sulli42 <dustinsullivan@msn.com>
2016-01-20 22:18:35 -0700

    Update TLD_mapping_tool_experimental.py

--- TLD_mapping_tool_experimental.py
+++ TLD_mapping_tool_experimental.py
@@ -72,8 +72,8 @@ def deleteScreenshots(path):
 
 def contourPlot(data, path, save=True):
     fig = plt.figure()
-    xi = linspace(min(data[:,0]),max(data[:,0]),141) 
-    yi = linspace(min(data[:,2]),max(data[:,2]),141)
+    xi = linspace(min(data[:,0]),max(data[:,0]),221) 
+    yi = linspace(min(data[:,2]),max(data[:,2]),221)
     zi = griddata(data[:,0],data[:,2],data[:,1], xi,yi, interp='linear') 
     #zi = nd.gaussian_filter(zi, sigma=0.6, order=0)
     plt.contour (xi,yi,zi,41,linewidths=0.25,colors='saddlebrown') 
