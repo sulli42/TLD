e5e213b17f3c5386f9985c83debd52ba16779551
sulli42 <dustinsullivan@msn.com>
2016-01-20 19:34:58 -0700

    Update TLD_mapping_tool_experimental.py

--- TLD_mapping_tool_experimental.py
+++ TLD_mapping_tool_experimental.py
@@ -76,8 +76,8 @@ def contourPlot(data, path, save=True):
     yi = linspace(min(data[:,2]),max(data[:,2]),111)
     zi = griddata(data[:,0],data[:,2],data[:,1], xi,yi, interp='linear') 
     #zi = nd.gaussian_filter(zi, sigma=0.6, order=0)
-    plt.contour (xi,yi,zi,41,linewidths=1,colors='black') 
-    plt.contourf(xi,yi,zi,1);
+    plt.contour (xi,yi,zi,40,linewidths=0.5,colors='black') 
+    plt.contourf(xi,yi,zi,0);
     plt.colorbar()  
     plt.grid(True)
     plt.set_cmap('terrain')
